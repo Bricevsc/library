2dbc5893e6916ecad870597930c4479820b36c48
brice <prodakil@gmail.com>
2023-06-19 11:44:54 +0200

    ✅

--- middlewares/validateBook.js
+++ middlewares/validateBook.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 export default (req, res, next) => {
   const addBookSchema = Joi.object({
     name: Joi.string().required,
+    author: Joi.string().required,
   });
   const { error } = addBookSchema.validate(req.body, { abortEarly: false });
   if (error) return res.status(400).send;
